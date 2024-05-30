c01d6edbbc282f532d70036db35521f9d900665f
Mohamed-Farah01 <160015556+Mohamed-Farah01@users.noreply.github.com>
2024-05-30 13:23:23 +0400

    Add files via upload

---
+++ product.js
@@ -0,0 +1,57 @@
+let products = [
+    {
+        "id": 1001,
+        "title": 'Cat food, 25lb bag',
+        "description": 'A 25 pound bag of <em>irressistible</em>, organic goodness for your cat.',
+        "price": 20.00,
+        "image": 'Image/catFood (1).jpeg',
+        // note, for available inventory,in your CW, set it to 10
+        "availableInventory": 10, // was '5'
+        "rating": 2
+    },
+    
+    {
+        "id": 1002,
+        "title": 'Yarn',
+        "description": 'Yarn your cat and play with for a very <strong>long</strong> time!',
+        "price": 2.99,
+        "image": 'Image/yarn (1).jpeg',
+        // note, for available inventory,in your CW, set it to 10
+        "availableInventory": 7, // was '5'
+        "rating": 3
+    },
+    
+    {
+        "id": 1003,
+        "title": 'Kitty Litter',
+        "description": 'A 25 pound bag of <em>irressistible</em>, organic goodness for your cat.',
+        "price": 20.00,
+        "image": 'Image/kittyLitter (1).jpeg',
+        // note, for available inventory,in your CW, set it to 10
+        "availableInventory": 10, // was '5'
+        "rating": 2
+    },
+    
+    {
+        "id": 1004,
+        "title": 'Cat House',
+        "description": 'A place for your cat to play!',
+        "price": 7.99,
+        "image": 'Image/catHouse (1).jpeg',
+        // note, for available inventory,in your CW, set it to 10
+        "availableInventory": 11, // was '5'
+        "rating": 5
+    },
+    
+    {
+        "id": 1005,
+        "title": 'Laser Pointer',
+        "description": 'Drive your cat crazy with this <em>amazing</em> product.',
+        "price": 49.99,
+        "image": 'Image/laserPointer (1).jpeg',
+        // note, for available inventory,in your CW, set it to 10
+        "availableInventory": 25, // was '5'
+        "rating": 4
+    },
+    ]
+        
---
+++ service-worker.js
@@ -0,0 +1,41 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+    'index.html',
+    'product.js',
+    'petstore.webmanifest',
+    'Image/yarn (1).jpeg',
+    'Image/kittyLitter (1).jpeg',
+    'Image/laserPointer (1).jpeg',
+    'Image/catHouse (1).jpeg',
+    'Image/catFood (1).jpeg',
+    'Image/icon-store-512.png'
+];
+
+self.addEventListener('install', (e) =>{
+    console.log('[Service Worker] Install]');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) =>{
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
+
+});
+
+self.addEventListener('fetch', function(e){
+    e.respondWith(
+        caches.match(e.request).then(function(r){
+            // Download the file if it is not in the cache
+            return r || fetch (e.request).then(function(response){
+            // Add the new file to cache
+            return caches.open(cacheName).then(function(cache){
+                cache.put(e.request, response.clone());
+                return response;
+            });
+       
+            });
+        
+        })
+    
+    );
+});
